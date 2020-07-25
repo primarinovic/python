06e039bc0189ec53aa2f656bde3c66a50fd009e6
Priscila Santos Marinovic <46497850+primarinovic@users.noreply.github.com>
2020-07-25 09:14:53 -0300

    Delete Exercício 38 – Comparando números.py

--- Exercício 38 – Comparando números.py
+++
@@ -1,9 +0,0 @@
-n1 = int(input('Primeiro número: '))
-n2 = int(input('Segundo número: '))
-
-if n1 > n2:
-    print('O primeiro valor é maior.')
-elif n1 < n2:
-    print('O segundo número é maior.')
-else:
-    print('Os números são iguais.')
